c71a2e617b4f0531a51738b4bb288b144a154df5
aloaam <aloaam@gmail.com>
2023-05-16 18:50:24 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./assets/walletBuddyLogo.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} alt="React logo" />
+      </div>
+      <h1>
+        This page is just a personal project. No revenue is generated out of it.
+      </h1>
+
+      <p className="read-the-docs">Coming Soon!</p>
+    </>
+  );
+}
+
+export default App;
