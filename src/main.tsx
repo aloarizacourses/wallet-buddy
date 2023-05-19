0bea78e094907f98fc7059e659316b337dd9e60c
aloaam <aloaam@gmail.com>
2023-05-19 15:23:56 +0200

    set up light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
-const colors = {
-  chakra: {
-    body: {
-      bg: "#fffff",
-    },
-  },
-};
-
-const theme = extendTheme({ colors });
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
--- src/theme.ts
+++ src/theme.ts
@@ -1,2 +1,9 @@
-import { extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
